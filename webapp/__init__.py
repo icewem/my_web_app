3b8698f84f7426ae799e9a8b65eb149934621e60
icewem <icewem@gmail.com>
2019-04-10 19:17:55 +0300

    добавил вывод из поиска на экран

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,15 +3,16 @@ from webapp.forms import SearchForm
 
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
 
 @app.route("/", methods = ['GET', 'POST'])
 def main():
-    myFormObject = SearchForm
+    form = SearchForm()
+    if form.validate_on_submit():
+        return render_template('index.html', page_title="Good", form=form)
+    return render_template('index.html', page_title="Форму обошли", form=form)
 
-    if request.method == 'GET':
-        return render_template('index.html', page_title="Форма Работает", form=myFormObject)
-
-    return render_template('index.html', page_title="Обошли форму")
 
 
 if __name__ == "__main__":
--- webapp/forms.py
+++ webapp/forms.py
@@ -3,5 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
-    search = StringField('Текст поиска', validators=[DataRequired("Введите значение для поиска")])
-    submit = SubmitField('Искать')
+
+    search = StringField('Текст поиска', validators=[DataRequired()], render_kw={"class": "form-control mr-sm-2", "type": "search", "placeholder": "Search", "aria-label": "Search"})
+    submit = SubmitField('Искать', render_kw={"class":"btn btn-outline-success my-2 my-sm-0", "type":"submit"})
